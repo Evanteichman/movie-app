d282666b0038610d75db4d78bd3c6621284e0ce4
Evanteichman <81187144+Evanteichman@users.noreply.github.com>
2021-04-28 14:37:20 -0700

    Update AppRouter.js

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,36 +1,28 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from '../components/Header';
-
 import PageAbout from '../pages/PageAbout';
 import PageHome from '../pages/PageHome';
 import Footer from '../components/Footer';
 import PageNotFound from '../pages/PageNotFound';
 import '../scss/main.scss';
-
 import Favourites from '../pages/Favourites';
 import MovieInfo from '../pages/MovieInfo'
-
-
-
 function App() {
   return (
-
     <Router>
       <div className="App">
+         <Header />
         <Switch>
-          <Header />
           <Route path="/" exact><PageHome /></Route>
           <Route path="/favourites"> <Favourites /></Route>
           <Route path="/movieinfo"><MovieInfo /></Route>
           <Route path="/about"><PageAbout /></Route>
           <Route><PageNotFound /></Route>
         </Switch>
-      <PageHome />
+      {/* <PageHome /> */}
       <Footer />
-
       </div>
    </Router>
   );
 }
-
 export default App;
