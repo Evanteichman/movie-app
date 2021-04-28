2bcb6b133b3c28ee3f831b590c94fa5ee1f7a21b
kaarenc <kcorrigan8@my.bcit.ca>
2021-04-28 13:33:38 -0700

    Adding AppRouter changes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,14 +1,17 @@
 import '../scss/main.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 import Header from '../components/Header';
+import Favourites from '../pages/Favourites';
+import MovieInfo from '../pages/MovieInfo'
 
 function App() {
   return (
     <Router>
     <div className="App">
       <Header />
-      
+      <Route path="/favourites">
+      <Favourites /></Route>
+      <Route path="/movieinfo"><MovieInfo /></Route>
 
     </div>
     </Router>
