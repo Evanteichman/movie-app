b3d46893c5def8f4c20526d40f3c3cbaf73ef94f
ElenaDianaUngureanu <81187070+ElenaDianaUngureanu@users.noreply.github.com>
2021-04-28 13:52:58 -0700

    page home modified

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,26 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from '../components/Header';
 import PageAbout from '../pages/PageAbout';
+import PageHome from '../pages/PageHome';
 import Footer from '../components/Footer';
 import PageNotFound from '../pages/PageNotFound';
 import '../scss/main.scss';
 
 function App() {
   return (
+    
     <Router>
       <div className="App">
         {/* <Header />
         <Nav /> */}
         <Switch>
-        <Header />
+          <Header />
+          <Route path="/" exact><PageHome /></Route>
           <Route path="/about"><PageAbout /></Route>
           <Route><PageNotFound /></Route>
         </Switch>
+        <PageHome />
+        {/* <PageAbout /> */}
         <Footer />
       </div>
     </Router>
