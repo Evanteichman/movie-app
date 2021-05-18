83f6f756dfb5347ef03dac995d4878572e28229b
Evanteichman <81187144+Evanteichman@users.noreply.github.com>
2021-05-18 13:44:28 -0700

    updated header links, updated slideshow styling

---
+++ src/pages/SearchResults.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import Searchbox from '../components/Searchbox';
+import Header from '../components/Header';
+
+function SearchResults({ movies }) {
+    // const [movies, setMovies] = useState([]);
+
+    return (
+        <div>
+            {/* {movies.map((movies, index) => (<div>
+                <img className="poster" src={`https://image.tmdb.org/t/p/w500/${movies.poster_path}`} alt="Movie Poster" />
+            </div>))} */}
+        </div>
+    )
+}
+
+
+export default SearchResults;
