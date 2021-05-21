3cbe4c436be20bc3d0894529756e0bcecb1b695e
ElenaDianaUngureanu <81187070+ElenaDianaUngureanu@users.noreply.github.com>
2021-05-20 21:52:16 -0700

    new change

--- src/pages/MovieInfo.js
+++ src/pages/MovieInfo.js
@@ -53,50 +53,44 @@ const MovieInfo = () => {
     return (
 
         <div className="wrapper">
-            {movieData !== null &&
-                <div className="movie-info-wrapper">
-                    <div className="movie-poster">
-                        {movieData.poster_path === null ?
-                            <img className="single-poster" src={noPoster} alt="No poster available." /> :
-                            <img className="single-poster" src={`https://image.tmdb.org/t/p/w500/${movieData.poster_path}`} alt="Movie Poster" />
-                        }
-                        <FavouriteHeart className="movieinfo-fav" movie={movieData} />
-                        {/* <img className="heart-single" src={HeartIcon} alt="filled heart icon"/> */}
-                        <section className="movie-single-title">
-                            <h2 className="title">{movieData.title}</h2>
-                            <h2 className="rating">{movieData.vote_average}</h2>
-                        </section>
+        {movieData !== null && 
+        <div className="movie-info-wrapper">
+            <div className="movie-poster">
+                {movieData.poster_path === null ?
+                <img className="single-poster" src={noPoster} alt="No poster available." /> : 
+                <img className="single-poster" src={`https://image.tmdb.org/t/p/w500/${movieData.poster_path}`} alt="Movie Poster" />
+                }
+                <FavouriteHeart className="movieinfo-fav" movie={movieData}/>
+                {/* <img className="heart-single" src={HeartIcon} alt="filled heart icon"/> */}
+
+            </div>
+            <section className="movie-single-title">
+                <h2 className="title">{movieData.title}</h2>
+                <h2 className="rating">{movieData.vote_average}</h2>
+            </section>            
+            <div className="info">
+                <section className="overview">
+                {movieData.overview == null ? <p>No Overview Available</p> : <p>{movieData.overview}</p> }
+                </section> 
+                <section className="details">  
+                    <div>
+                        <h3 className="release-date-title"> Release Date:</h3>
+                        {movieData.release_date == null ? <p>No Release Date</p> : <p className="release-date">{movieData.release_date} minutes</p>} 
                     </div>
-                    <div className="info">
-                        <section className="overview">
-                            <p>{movieData.overview}</p>
-                        </section>
-                        <section className="details">
-                            <div>
-                                <h3 className="release-date-title"> Release Date:</h3>
-                                <p className="date">{movieData.release_date}</p>
-                            </div>
-                            <div>
-                                <h3 className="run-time-title"> Run Time:</h3>
-                                <p className="run-time">{movieData.runtime} minutes</p>
-                            </div>
-                            <div>
-                                <h3 className="genre-title"> Genre(s):</h3>
-                                <p className="genre">{movieData.genres[0].name}</p>
-                            </div>
-
-                        </section>
-                        {/* <div className="production">
-                            <h3>Production:</h3>
-                            <p className="production-name">{movieData.production_companies[0].name}</p>
-                            <p className="production-name">{movieData.production_companies[1].name}</p>
-                        </div> */}
-
+                    <div>
+                        <h3 className="run-time-title"> Run Time:</h3>
+                        {movieData.runtime == null ? <p>No Runtime Available</p> : <p className="run-time">{movieData.runtime} minutes</p>} 
                     </div>
-                </div>
-            }
-
+                    <div>
+                        <h3 className="genre-title"> Genre(s):</h3>
+                        {!movieData.genres[0].name  ? <p>No Genre Available</p> : <p className="genre">{movieData.genres[0].name}</p>}
+                    </div>                    
+                </section>       
+            </div>
         </div>
+        }
+        
+    </div>
 
     );
 }
