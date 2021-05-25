ee5f184434c7d7b81e1ccab28efee6dbb6cda900
kaarenc <kcorrigan8@my.bcit.ca>
2021-05-24 20:14:01 -0700

    prep for npm build

--- src/globals/globals.js
+++ src/globals/globals.js
@@ -2,3 +2,4 @@ export const appTitle = 'Fresh Flix';
 export const endPointSearch = 'https://api.themoviedb.org/3/movie/550?api_key=d53a15d5d3ae63a24027095be1b24d94';
 export const apiKey = 'd53a15d5d3ae63a24027095be1b24d94';
 export const TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNTNhMTVkNWQzYWU2M2EyNDAyNzA5NWJlMWIyNGQ5NCIsInN1YiI6IjYwODljMTMzNTRhMDk4MDA1N2QxOWE4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.29HPRKorKZ_zaGNVhOcLJuZbf3iu9wDqZuUl83hKYME';
+export const APP_FOLDER_NAME= 'fresh-flix';
